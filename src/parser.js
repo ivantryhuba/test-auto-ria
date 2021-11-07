fec406c88a4a233dc6debf1fbfbbfc59b94474b6
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-11-07 16:34:12 +0200

    Fix Boolean

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,10 @@
-const parser = (testUrl) => {
+const parser = testUrl => {
   const url = new URL(testUrl);
 
   const params = {};
 
-  if(!validURL(url)){
-    throw new Error('Invalid URL')
+  if (!validURL(url)) {
+    throw new Error('Invalid URL');
   }
 
   url.searchParams.forEach((value, name) => {
@@ -16,57 +16,63 @@ const parser = (testUrl) => {
 
     if (isDot === -1) {
       if (value[0] === '"' && value[value.length - 1] === '"') {
-        return (params[name] = String(value.replace(/['"]+/g, '')));
+        return (params[name] = value.replace(/['"]+/g, ''));
       }
 
       if (!isNaN(+value)) {
         return (params[name] = Number(value));
       }
-      if (value === 'true' || value === 'false') {
-        return (params[name] = Boolean(value));
+
+      if (value === 'true') {
+        return (params[name] = true);
       }
 
-      return (params[name] = String(value));
+      if (value === 'false') {
+        return (params[name] = false);
+      }
+
+      return (params[name] = value);
     }
 
     const obj = name.split('.');
 
     if (value[0] === '"' && value[value.length - 1] === '"') {
-      // console.log(1, String(value));
       return (params[obj[0]] = {
         ...params[obj[0]],
-        [obj[1]]: String(value.replace(/['"]+/g, '')),
+        [obj[1]]: value.replace(/['"]+/g, ''),
       });
     }
 
     if (!isNaN(+value)) {
-      // console.log(2, Number(value));
       return (params[obj[0]] = { ...params[obj[0]], [obj[1]]: Number(value) });
     }
-    if (value === 'true' || value === 'false') {
-      // console.log(Boolean(value), value);
-      // console.log(3, Boolean(value));
-      return (params[obj[0]] = { ...params[obj[0]], [obj[1]]: Boolean(value) });
+    if (value === 'true') {
+      return (params[obj[0]] = { ...params[obj[0]], [obj[1]]: true });
     }
 
-    // console.log('obj', obj);
+    if (value === 'false') {
+      return (params[obj[0]] = { ...params[obj[0]], [obj[1]]: false });
+    }
+
     return (params[obj[0]] = { ...params[obj[0]], [obj[1]]: value });
   });
 
-  // console.log('Params', params);
-  console.log(JSON.stringify(params));
+  console.log('Params =', JSON.stringify(params, null, 2));
 
-  return Object.keys(params)[0] ? params : null
+  return Object.keys(params)[0] ? params : null;
 };
 
 function validURL(str) {
-  const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return !!pattern.test(str);
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  ); // fragment locator
+  return pattern.test(str);
 }
 
 module.exports = parser;
